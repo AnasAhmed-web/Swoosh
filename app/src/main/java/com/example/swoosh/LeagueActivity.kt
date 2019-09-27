d74c0ea513c9a50949baf56a9dc3df8b9432f246
AnasAhmed-web <alamareenanas78@gmail.com>
2019-09-27 08:14:31 +0300

    Logic Screen

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,15 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : Base_Activity() {
+    var selectedLeague = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+    fun mensLeagueClicked(view:View){
+        womensleguebtn.isChecked = false
+        coedleguebtn.isChecked = false
+        selectedLeague = "mens"
+    }
+    fun womensLeagueClicked(view:View){
+        mensleaguebtn.isChecked = false
+        coedleguebtn.isChecked = false
+        selectedLeague = "womens"
+    }
+    fun coedLeagueClicked(view: View){
+        mensleaguebtn.isChecked = false
+        womensleguebtn.isChecked = false
+        selectedLeague = "co-ed"
+    }
     fun nextLayout(view: View){
+        if(selectedLeague != ""){
         val skillIntent = Intent(this,SkillActivity::class.java)
+            skillIntent.putExtra(EXTRA_LEAGUE,selectedLeague)
+
         startActivity(skillIntent)
+        } else{
+            Toast.makeText(this,"Please Select League.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -1,12 +1,14 @@
 package com.example.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
-
+class SkillActivity : Base_Activity() {
+    var league = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
     }
 }
